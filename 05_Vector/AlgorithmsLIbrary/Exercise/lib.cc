c76f1541720364fafd85ce09e08ce2d54b5a47b9
christophJJmueller <christoph_mueller91@web.de>
2023-10-02 13:24:40 +0200

    Abschnitt 5 abgeschlossen

--- 05_Vector/AlgorithmsLIbrary/Exercise/lib.cc
+++ 05_Vector/AlgorithmsLIbrary/Exercise/lib.cc
@@ -6,17 +6,55 @@
 
 bool all_of(const std::vector<std::int32_t> &vector, const std::int32_t value)
 {
+    bool is_all_of = false;
+
+    for (auto &i : vector)
+    {
+        if(i == value)
+            is_all_of = true;
+        else
+            is_all_of = false;
+    }
+
+    return is_all_of;
 }
 
 bool any_of(const std::vector<std::int32_t> &vector, const std::int32_t value)
 {
+    bool is_any_of = false;
+
+    for (auto &i : vector)
+    {
+        if(i == value)
+            is_any_of = true;
+    }
+
+    return is_any_of;
 }
 
 bool none_of(const std::vector<std::int32_t> &vector, const std::int32_t value)
 {
+    bool is_none_of = true;
+
+    for (auto &i : vector)
+    {
+        if(i == value)
+            is_none_of = false;
+    }
+
+    return is_none_of;
 }
 
 std::size_t count(const std::vector<std::int32_t> &vector,
                   const std::int32_t value)
 {
+    std::size_t counter = 0;
+
+    for (auto &i : vector)
+    {
+        if(i == value)
+            counter++;
+    }
+
+    return counter;
 }
--- 05_Vector/VectorLibrary/Exercise/lib.cc
+++ 05_Vector/VectorLibrary/Exercise/lib.cc
@@ -6,20 +6,67 @@
 
 void remove_duplicates(std::vector<std::int32_t> &vector)
 {
+    for (std::size_t i = 0; i < vector.size(); i++)
+    {
+        auto duplicate_index = static_cast<std::size_t>(-1);
+        const auto current_value = vector[i];
 
+        for (std::size_t j = i + 1; j < vector.size(); j++)
+        {
+            if (current_value == vector[j])
+            {
+                duplicate_index = j;
+                break;
+            }
+        }
+
+        if (duplicate_index == static_cast<std::size_t>(-1))
+        {
+            continue;
+        }
+
+        for (std::size_t j = duplicate_index; j < vector.size(); j++)
+        {
+            vector[j] = vector[j + 1];
+        }
+
+        vector.pop_back();
+    }
 }
 
 void rotate_left(std::vector<std::int32_t> &vector)
 {
 
+    const auto first_element = vector.front();
+
+    for (std::size_t i = 1; i < vector.size(); i++)
+    {
+        vector[i-1] = vector[i];
+    }
+
+    vector.back() = first_element;
 }
 
 void rotate_right(std::vector<std::int32_t> &vector)
 {
 
+    std::int32_t last_value = vector.back();
+
+    for (std::size_t i = vector.size()-1; i > 0; i--)
+    {
+        vector[i] = vector[i-1];
+    }
+
+    vector.front() = last_value;
 }
 
 void swap_boundary_values(std::vector<std::int32_t> &vector)
 {
+    auto &front_value = vector.front();
+    auto &back_value = vector.back();
+
+    auto temp = front_value;
+    front_value = back_value;
+    back_value = temp;
 
 }
